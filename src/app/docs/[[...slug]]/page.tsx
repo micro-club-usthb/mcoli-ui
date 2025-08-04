167a15034a0dc17fb2fdf67b61b1f84fcb621850
Adel2411 <hadjarabadel.2411@gmail.com>
2025-08-04 01:59:15 +0100

    Added new UI fonts and dynamic metadata

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,20 +1,24 @@
-import * as React from 'react';
-import { notFound } from 'next/navigation';
+import * as React from "react";
+import { notFound } from "next/navigation";
 import {
   DocsBody,
   DocsDescription,
   DocsPage,
   DocsTitle,
-} from 'fumadocs-ui/page';
+} from "fumadocs-ui/page";
 
-import { source } from '@/lib/source';
-import { getMDXComponents } from '@/components/mdx';
+import { source } from "@/lib/source";
+import { getMDXComponents } from "@/components/mdx";
+import { Metadata, ResolvingMetadata } from "next";
 
-export default async function Page(props: {
+export default async function Page({
+  params,
+}: {
   params: Promise<{ slug?: string[] }>;
 }) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
+  const slug = (await params).slug;
+
+  const page = source.getPage(slug);
   if (!page) {
     notFound();
   }
@@ -23,6 +27,7 @@ export default async function Page(props: {
 
   return (
     <DocsPage toc={page.data.toc}>
+      <title>{`MC UI - ${page.data.title}`}</title>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
@@ -30,4 +35,23 @@ export default async function Page(props: {
       </DocsBody>
     </DocsPage>
   );
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug?: string[] }>;
+}): Promise<Metadata> {
+  const slug = (await params).slug;
+
+  // fetch post information
+  const page = source.getPage(slug);
+  if (!page) {
+    notFound();
+  }
+
+  return {
+    title: `MC UI - ${page.data.title}`,
+    description: page.data.description,
+  };
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans, DM_Sans } from "next/font/google";
 import { RootProvider } from "fumadocs-ui/provider";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
   subsets: ["latin"],
 });
 
@@ -48,7 +48,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSans.variable} ${plusJakartaSans.variable} antialiased font-primary`}
       >
         <RootProvider>{children}</RootProvider>
       </body>
