96a97d94574809c3d04a7d5411abb339ba80f8c9
Adel2411 <hadjarabadel.2411@gmail.com>
2025-10-12 16:59:45 +0100

    Fixed some import issues and fixed the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-5xl font-semibold">Hello world</h1>
+      <div className="flex flex-col items-center gap-10">
+        <h1 className="text-6xl font-bold">Hello world</h1>
+        <Link
+          href={"/docs/introduction"}
+          className="flex gap-1 text-xl font-medium hover:underline hover:text-gray-300"
+        >
+          Go to docs
+          <ArrowRight />
+        </Link>
+      </div>
     </div>
   );
 }
