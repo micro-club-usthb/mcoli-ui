02f8fe21ccfaa38a5a6f58eb2eedb0270b77251f
Adel2411 <hadjarabadel.2411@gmail.com>
2025-10-12 18:50:24 +0100

    improved some files formatting

--- src/registry/default/examples/floating-label-input-demo.tsx
+++ src/registry/default/examples/floating-label-input-demo.tsx
@@ -1,5 +1,5 @@
-import { FloatingInput } from '../ui/floating-label-input';
+import { FloatingInput } from "../ui/floating-label-input";
 
 export default function FloatingLabelInputDemo() {
-  return <FloatingInput label='Demo' />;
+  return <FloatingInput label="Demo" />;
 }
--- src/registry/default/ui/floating-label-input.tsx
+++ src/registry/default/ui/floating-label-input.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { cn } from '@/lib/utils';
-import { Label } from '@radix-ui/react-label';
-import { Input } from '@/components/ui/input';
+import { cn } from "@/lib/utils";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
 
 export const FloatingLabel: React.FC<React.ComponentProps<typeof Label>> = ({
   className,
@@ -11,7 +11,7 @@ export const FloatingLabel: React.FC<React.ComponentProps<typeof Label>> = ({
   return (
     <Label
       className={cn(
-        'peer-has-focus:secondary bg-background absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4',
+        "peer-has-focus:secondary bg-background absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
         className,
       )}
       {...props}
@@ -20,16 +20,16 @@ export const FloatingLabel: React.FC<React.ComponentProps<typeof Label>> = ({
 };
 
 export const FloatingInput: React.FC<
-  React.ComponentProps<'input'> & {
+  React.ComponentProps<"input"> & {
     label: string;
   }
 > = ({ id, label, className, ...props }) => {
   return (
-    <div className='relative'>
+    <div className="relative">
       <Input
         id={id}
-        className={cn('peer bg-background', className)}
-        placeholder=' '
+        className={cn("peer bg-background", className)}
+        placeholder=" "
         {...props}
       />
       <FloatingLabel htmlFor={id}>{label}</FloatingLabel>
