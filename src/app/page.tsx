229209d1563bf3349d809c07d23eee75fc22b741
Adel HB <hadjarabadel.2411@gmail.com>
2025-11-02 17:44:51 +0300

    Initialized all figma 1st configurations (need to test them now)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col items-center gap-10">
-        <h1 className="text-6xl font-bold">Hello world</h1>
+        <div className="space-y-1">
+          <h1 className="header-lg font-bold">Hello world</h1>
+          <p className="paragraph-md">
+            This is a test paragraph to test new styles
+          </p>
+        </div>
         <Link
           href={"/docs/introduction"}
           className="flex gap-1 text-xl font-medium hover:underline hover:text-primary-700"
