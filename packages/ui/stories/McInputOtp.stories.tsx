df1813d21da65dbe892af635774f18af489c52e1
berkal749 <berkalachrefpro@gmail.com>
2026-03-31 21:43:33 +0100

    feat: add mc-input-otp component

--- packages/ui/stories/McInputOtp.stories.tsx
+++ packages/ui/stories/McInputOtp.stories.tsx
@@ -14,7 +14,7 @@ type Story = StoryObj<typeof InputOtp>;
 
 export const Playground: Story = {
   render: () => (
-    <div className="flex items-center justify-center min-h-screen w-full bg-background">
+    <div className="flex flex-col items-center gap-2 justify-center min-h-screen w-full bg-background">
       <InputOtp maxLength={6}>
         <InputOTPGroup>
           <InputOTPSlot index={0} />
@@ -28,6 +28,36 @@ export const Playground: Story = {
           <InputOTPSlot index={5} />
         </InputOTPGroup>
       </InputOtp>
+
+      <InputOtp maxLength={6}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOtp>
+
+       <InputOtp maxLength={6}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+           </InputOTPGroup>
+           <InputOTPSeparator />
+           <InputOTPGroup>  
+           <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOtp>
+
+
     </div>
   ),
 };
