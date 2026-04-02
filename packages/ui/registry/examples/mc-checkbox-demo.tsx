240ad3087c2be5a81a7c8948146c10ef109d8d16
MrmAya <kahlimeriemaya@gmail.com>
2026-04-02 17:01:23 +0200

    refactor: add support text to the demo

--- packages/ui/registry/examples/mc-checkbox-demo.tsx
+++ packages/ui/registry/examples/mc-checkbox-demo.tsx
@@ -1,5 +1,5 @@
 import { McCheckbox } from "../ui/mc-checkbox";
 
 export default function McCheckboxDemo() {
-  return <McCheckbox text="Checkbox" />;
+  return <McCheckbox text="Checkbox" supportText="support Text"/>;
 }
